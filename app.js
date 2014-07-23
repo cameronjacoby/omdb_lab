bee132f072370d687b90f7b81e0505a9e3d4aa83
cameronjacoby <cameron.jacoby@gmail.com>
2014-07-23 00:42:50 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,99 @@
+var express = require('express'),
+  ejs = require('ejs'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  request = require('request');
+  
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('site/index.ejs');
+});
+
+app.get('/search', function(req, res) {
+  var query = req.query.searchTerm;
+  var url = 'http://www.omdbapi.com/?s=' + query;
+
+  request(url, function(error, response, body) {
+    if (!error) {
+      var data = JSON.parse(body);
+      res.render('movies/results.ejs', {movieList: data.Search || []});
+    }
+  });
+});
+
+app.get('/movie/:id', function(req, res) {
+  var movieID = req.params.id;
+  var url = 'http://www.omdbapi.com/?i=' + movieID;
+  request(url, function(error, response, body) {
+    if (!error) {
+      var data = JSON.parse(body);
+      res.render('movies/details.ejs', {movie: data});
+    }
+  });
+});
+
+var savedMovies = [];
+
+app.get('/watch-later', function(req, res) {
+  res.render('movies/watch_later', {savedMovies: savedMovies});
+});
+
+app.post('/watch-later', function(req, res) {
+  savedMovies.push(req.body.movie);
+  res.redirect('/watch-later');
+});
+
+app.get('/movie/quick/:id', function(req, res) {
+  var movieID = req.params.id;
+  var url = 'http://www.omdbapi.com/?i=' + movieID;
+  request(url, function(error, response, body) {
+    if (!error) {
+      var data = JSON.parse(body);
+      res.render('movies/quick_details.ejs', {movie: data});
+    }
+  });
+});
+
+app.delete('/movie/quick/:id', function(req, res){
+  var movieID = Number(req.params.id);
+  var movieIndex;
+  savedMovies.forEach(function(movie, index){
+     if(movie.imdbID === movieID) {
+       movieIndex = index;
+     }
+  });
+  savedMovies.splice(movieIndex, 1);
+  res.redirect('/watch-later');
+});
+
+app.listen(3000, function() {
+  console.log('server started on localhost:3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
